fd6dcf80379d4942564c57de211a35d44f2345ec
Alexander Erofeev <61013758+k0dokushi@users.noreply.github.com>
2021-12-16 22:09:11 +0400

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,8 @@ int main()
 	sf::ContextSettings settings;
 	settings.antialiasingLevel = 8;
 
-	sf::RenderWindow window(sf::VideoMode(1294, 712), "Conway's Game of Life", sf::Style::Close | sf::Style::Titlebar, settings);
+	sf::RenderWindow window(sf::VideoMode(1291, 712), "Conway's Game of Life", sf::Style::Close | sf::Style::Titlebar, settings);
+	window.setFramerateLimit(60);
 	cr::game_controller game(window, 76, 50, 12);
 	game.start();
 
