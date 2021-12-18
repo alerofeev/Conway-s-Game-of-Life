6c77f77902f32d03f6294f022cf7c250bc491d9b
Alexander Erofeev <61013758+k0dokushi@users.noreply.github.com>
2021-12-18 23:10:43 +0400

    181221

--- src/Conway-s-Game-of-Life-consistent.cpp
+++ src/Conway-s-Game-of-Life-consistent.cpp
@@ -3,12 +3,12 @@
 #include <vector>
 #include <thread>
 
-namespace cr
+namespace ca
 {
-	class engine
+	class cellular_automaton
 	{
 	public:
-		engine(const int i, const int j)
+		cellular_automaton(const int i, const int j)
 			: current_state_(std::vector<std::vector<char>>(i, std::vector<char>(j, 0))),
 			next_state_(std::vector<std::vector<char>>(i, std::vector<char>(j, 0))) { }
 
@@ -48,9 +48,9 @@ namespace cr
 			for (auto& position : neighbors_position_)
 			{
 				if (current_state_[i + position[0] < 0 ? current_state_.size() - 1 :
-					i + position[0] > static_cast<int>(current_state_.size()) - 1 ? 0 : i + position[0]][j + position[1] < 0 ?
-					static_cast<int>(current_state_[0].size()) - 1 : j + position[1] > static_cast<int>(current_state_[0].size()) - 1 ? 0 :
-					j + position[1]] == 1)
+					i + position[0] > static_cast<int>(current_state_.size()) - 1 ? 0 : i + position[0]]
+					[j + position[1] < 0 ? static_cast<int>(current_state_[0].size()) - 1 : 
+					j + position[1] > static_cast<int>(current_state_[0].size()) - 1 ? 0 : j + position[1]] == 1)
 				{
 					neighbors_counter++;
 				}
@@ -65,20 +65,20 @@ namespace cr
 int main()
 {
 	constexpr int rows_counter = 100, columns_counter = 100;
-	constexpr int steps_counter = 100;
+	constexpr int steps_counter = 600;
 
-	cr::engine engine(rows_counter, columns_counter);
+	ca::cellular_automaton cellular_automaton(rows_counter, columns_counter);
 
-	engine.set_cell_state(4, 6, 1);
-	engine.set_cell_state(4, 7, 1);
-	engine.set_cell_state(5, 5, 1);
-	engine.set_cell_state(5, 6, 1);
-	engine.set_cell_state(6, 6, 1);
+	cellular_automaton.set_cell_state(4, 6, 1);
+	cellular_automaton.set_cell_state(4, 7, 1);
+	cellular_automaton.set_cell_state(5, 5, 1);
+	cellular_automaton.set_cell_state(5, 6, 1);
+	cellular_automaton.set_cell_state(6, 6, 1);
 	
 	const auto start = std::chrono::high_resolution_clock::now();
 	for (int iterator = 0; iterator < steps_counter; iterator++)
 	{
-		engine.make_step();
+		cellular_automaton.make_step();
 	}
 	const auto end = std::chrono::high_resolution_clock::now();
 
